fd36115b17c70554f82897e252fabf8ee7ea567b
Sébastien Braun <sebbraun@gmail.com>
2019-03-06 18:24:54 +0100

    remote enhanced backend introduced

--- main.tf
+++ main.tf
@@ -1,21 +1,34 @@
+# Using a single workspace:
+terraform {
+  backend "remote" {
+    hostname     = "replicated.yet.org"
+    organization = "yet"
+
+    workspaces {
+      name = "terraform-aws-pov-instance"
+    }
+  }
+}
+
 provider aws {
-  region     = "${var.aws_region}"
-  version    = "~> 1.50"
+  region  = "${var.aws_region}"
+  version = "~> 1.50"
 }
 
 data "terraform_remote_state" "vpc" {
   backend = "atlas"
+
   config {
-    name = "yet/terraform-aws-vpc"
+    name    = "yet/terraform-aws-vpc"
     address = "https://replicated.yet.org"
   }
 }
 
 resource "aws_instance" "pov_instance" {
-  ami               = "${var.ami_id}"
-  instance_type     = "${var.instance_type}"
+  ami           = "${var.ami_id}"
+  instance_type = "${var.instance_type}"
 
-  subnet_id         = "${element(data.terraform_remote_state.vpc.private_subnets, 0)}"
+  subnet_id = "${element(data.terraform_remote_state.vpc.private_subnets, 0)}"
 
   tags {
     Name = "${var.instance_tag}"
--- output.tf
+++ output.tf
@@ -1,8 +1,8 @@
-output "public_dns" {
-  value = "${aws_instance.pov_instance.public_dns}"
+output "private_ip" {
+  value = "${aws_instance.pov_instance.private_ip}"
 }
 
 output "vpc_id" {
   description = "VPC ID where to launch our instance."
-  value = "${data.terraform_remote_state.vpc.vpc_id}"
+  value       = "${data.terraform_remote_state.vpc.vpc_id}"
 }
