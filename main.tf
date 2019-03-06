d4d69c2a6a191bf80a54fe396463187d0ca6ee80
Sébastien Braun <sebbraun@gmail.com>
2019-03-06 16:51:55 +0100

    removed optional AZ

--- main.tf
+++ main.tf
@@ -6,7 +6,6 @@ provider aws {
 resource "aws_instance" "pov_instance" {
   ami               = "${var.ami_id}"
   instance_type     = "${var.instance_type}"
-  availability_zone = "${var.aws_region}"
 
   subnet_id         = "subnet-03d9b5176be0f546b"
 
