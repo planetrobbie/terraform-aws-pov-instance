317acc577eb4fe1c3441636a3d624d2849467dbe
Sébastien Braun <sebbraun@gmail.com>
2019-03-06 17:04:51 +0100

    vpc_id output

--- output.tf
+++ output.tf
@@ -1,3 +1,8 @@
 output "public_dns" {
   value = "${aws_instance.pov_instance.public_dns}"
 }
+
+output "vpc_id" {
+  description = "VPC ID where to launch our instance."
+  value = "${data.terraform_remote_state.vpc.vpc_id}"
+}
