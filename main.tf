f7f5f8f42126caa7aee12e2bc4531d22d95fd9bb
Sébastien Braun <sebbraun@gmail.com>
2019-03-06 17:02:44 +0100

    grabbing VPC id from other workspace

--- main.tf
+++ main.tf
@@ -3,6 +3,14 @@ provider aws {
   version    = "~> 1.50"
 }
 
+data "terraform_remote_state" "vpc" {
+  backend = "atlas"
+  config {
+    name = "yet/terraform-aws-vpc"
+    address = "https://replicated.yet.org"
+  }
+}
+
 resource "aws_instance" "pov_instance" {
   ami               = "${var.ami_id}"
   instance_type     = "${var.instance_type}"
